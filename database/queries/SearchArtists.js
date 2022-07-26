c318bd6bdfd9c22e01608e76360c29621f2ba8b7
omar2004712 <o.bahutar@gmail.com>
2022-07-26 17:19:44 +0400

    feat (SearchArtists): added a searching by name feature
    used $test query to search for the name
    $test requires search index therefore the index was added

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Searches through the Artist collection
@@ -9,4 +9,48 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()]).then((result) => {
+    return {
+      all: result[0],
+      count: result[1],
+      offset,
+      limit,
+    };
+  });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+    // to use the above command
+    // must create an index for the searched property in this case the name
+    // to create the index
+    // 1 - open the mongo shell on the terminal using the "mongo" command
+    // if it's not working make sure you add it to the environment variables
+    // 2 - switch to the desired database
+    // 3 - type: "db.<the name of the collection in this case artists">.createIndex({ <the property name>: <type of index in this case text>})
+    // done
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max,
+    };
+  }
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max,
+    };
+  }
+
+  return query;
 };
